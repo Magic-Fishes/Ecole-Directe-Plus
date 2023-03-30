cb136ac0fc68d294a9b8b22e497351f6e9964ce7
Truite Séchée <66438253+Truiteseche@users.noreply.github.com>
2023-03-30 18:28:31 +0000

    Gitignore update ; Window component update & hoverNotification creation

--- src/components/Window.jsx
+++ src/components/Window.jsx
@@ -1,3 +1,12 @@
+import { useState } from "react"
+
 export default function Window() {
-    return <div class="window"></div>
+    // States
+    let title = useState("default title");
+    let content = useState(<div id="id"><p>Window content</p></div>);
+
+    // Behavior
+
+    // JSX
+    return <div class="window"><div class="windowTitle">{title}</div><div id="windowContent">{content}</div></div>
 }
---
+++ src/components/hoverNotification.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+
+export default function hoverNotification() {
+    // States
+    let content = useState(<div id="id"><p>Notification's content</p></div>);
+
+    // Behavior
+    // if parentElement is hover {appear()}
+
+    // JSX
+    return <div class="hoverNotification"><div id="windowContent">{content}</div></div>
+}
