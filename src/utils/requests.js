4a465d9ae830e713a1be964c0d5e78460b2e319a
Truite Séchée <truite.berthail@gmail.com>
2024-02-12 18:38:04 +0100

    fix(proxy): switching proxy provider (very temporary fix)

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -1,5 +1,5 @@
 
 export function getProxiedURL(url) {
-    const proxyURL = "https://ecole-directe.plus:3000/proxy?url=";
+    const proxyURL = "https://raspi.ecole-directe.plus:3000/proxy?url=";
     return proxyURL + encodeURIComponent(url);
 }
