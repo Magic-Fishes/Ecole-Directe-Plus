c013a483db8524d588965f13959a02cfdc4b632e
saumon-brule <75emilien75@gmail.com>
2024-02-12 19:32:37 +0100

    fix: upgrading feedback privacy

--- src/components/Feedback/FeedbackForm.jsx
+++ src/components/Feedback/FeedbackForm.jsx
@@ -296,7 +296,7 @@ export default function FeedbackForm({ activeUser, carpeConviviale, onSubmit=() 
                         {
                             color: parseInt("0x" + color),
                             author: {
-                                name: (activeUser ? activeUser.lastName + " " + activeUser.firstName : "Poisson-zèbre Augmenté") + " (" + (isAnonymous ? "N/A" : (userEmail || (activeUser ? activeUser.email : ""))) + ")",
+                                name: ((activeUser && isAnonymous) ? activeUser.lastName + " " + activeUser.firstName : "Poisson-zèbre Augmenté") + " (" + (isAnonymous ? "N/A" : (userEmail || (activeUser ? activeUser.email : ""))) + ")",
                                 icon_url: ((isAnonymous || !activeUser) ? "https://i.ibb.co/CKmD9z8/poisson-z-bre.jpg" : getProxiedURL("https:" + activeUser.picture))
                             },
                             title: "**__" + selectedFeedbackType + "__ : " + subject + "**",
