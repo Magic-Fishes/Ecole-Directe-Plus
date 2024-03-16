5a45b78a14aba808ff7dbb0e7c6b38d297718a74
saumon-brule <96207807+saumon-brule@users.noreply.github.com>
2024-03-16 20:42:47 +0100

    feat(getBrowser) : new function to detect the user's browser

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -119,3 +119,16 @@ export function downloadFile(blobFile, filename) {
     // Clean up by revoking the Blob URL
     URL.revokeObjectURL(url);
 }
+
+export function getBrowser() { // I didn't check all browsers, see : https://developer.mozilla.org/en-US/docs/Web/HTTP/Browser_detection_using_the_user_agent#browser_name_and_version
+    const UA = navigator.userAgent
+    return (
+        (UA.includes("OPR/") || UA.includes("Opera/")) ? "Opera" : // verified on my computer
+        (UA.includes("Chromium/")) ? "Chromium" : // not verified
+        (UA.includes("SeaMonkey/")) ? "Seamonkey" : // I honestly hope people don't use this anymore
+        (UA.includes("Firefox/")) ? "Firefox" : // verified on my computer
+        (UA.includes("Edg/")) ? "Edge" : // verified on my computer
+        (UA.includes("Chrome/")) ? "Chrome" : // verified on my computer
+        "Safari" // not verified
+    )
+}
