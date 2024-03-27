3c91157d65cf83237b9bc099592bfa94072c1afb
Truite Séchée <truite.berthail@gmail.com>
2024-03-27 21:38:23 +0100

    Update policy

--- src/components/generic/Policy.jsx
+++ src/components/generic/Policy.jsx
@@ -11,21 +11,23 @@ import "./Policy.css";
 export default function Policy({ onCloseNavigateURL }) {
     const navigate = useNavigate();
 
+    // randomize the appariton order of the lead developpers (credits)
     const leadDeveloppers = ["Saumon Brulé", "Truite Séchée"];
     const [firstLeadDeveloppersDisplayedIdx, setFirstLeadDevelopperDisplayedIdx] = useState(Math.floor(Math.random() * 2));
-    
+
     const legalNotice = <ul id="legal-notice">
-        <h3 className="part-header">Résumé</h3>
-<ul>
-        <h4 className="sub-header">❌ Ecole Directe Plus ne collecte aucune information sur les utilisateurs du service </h4>
-        <h4 className="sub-header">❌ Ecole Directe Plus ne créé pas de compte lors de la connection, la connexion à lieu sur les serveurs d'Aplim, nous ne STOCKONS PAS les identifiants des personnes se connectant.</h4>
-        <h4 className="sub-header">❌ Ecole Directe Plus ne permet, ni ne prétend donner accès à des données auxquelles l'élève n'a pas accès incluant, mais ne se limitant pas aux : points aux examens* et au rang de l'élève*.</h4>
-        <h4 className="sub-header"> ℹ️ Les seules données collectées le sont par Aplim <a href="https://www.ecoledirecte.com" target="_blank">(EcoleDirecte)</a> conformément à leur politique de confidentialité décrite dans les <a href="https://www.ecoledirecte.com/login" target="_blank">Mentions Légales</a></h4>
-        <li className="legal-content">*Si l'accès à ses données est possible par l'utilisateur sur la plateforme classique d'EcoleDirecte, ces données peuvent être affichées sur Ecole Directe Plus. De plus, si elles ne sont pas disponibles, les moyennes de l'utilisateur seront calculés, mais pas conservés sur nos serveurs. </li>
-</ul>
 
         <h3 className="part-header">Politique de confidentialité</h3>
-        <h4 className="sub-header">Ecole Directe Plus utilise des données qui sont fournis par <a href="https://www.ecoledirecte.com" target="_blank">EcoleDirecte</a>, héritant ainsi de leur politique de confidentialité qui sont présentées ci-dessous :</h4>
+        <h4 className="sub-header">Résumé</h4>
+        <ul>
+            <p className="legal-content">✅ Ecole Directe Plus ne collecte aucune information sur les utilisateurs du service.</p>
+            <p className="legal-content">✅ Ecole Directe Plus ne crée pas de compte lors de la connexion, la connexion a lieu sur les serveurs d'Aplim. Autrement dit, nous ne STOCKONS PAS les identifiants des utilisateurs se connectant.</p>
+            <p className="legal-content">✅ Ecole Directe Plus ne permet, ni ne prétend donner accès à des données auxquelles l'élève n'a pas accès, incluant, mais ne se limitant pas aux : points aux examens* et au rang de l'élève*.</p>
+            <p className="legal-content"> <span id="info-emoji">ℹ️</span> Les seules données collectées le sont par Aplim (<a className="external-link" href="https://www.ecoledirecte.com" target="_blank">EcoleDirecte</a>) conformément à leur politique de confidentialité décrite dans les <a className="external-link" href="https://www.ecoledirecte.com/login" target="_blank">Mentions Légales.</a></p>
+            <p className="legal-content">*Si l'accès à ces données est possible par l'utilisateur sur la plateforme classique d'EcoleDirecte, ces données peuvent être affichées sur Ecole Directe Plus. Par ailleurs, si les moyennes de l'utilisateur ne sont pas disponibles, elles seront calculées, mais ce de façon locale sur l'appareil du client, les informations ne sont PAS transmises à nos serveurs.</p>
+        </ul>
+        <hr className="light-hr" />
+        <h4 className="sub-header">Ecole Directe Plus est un service non-officiel utilisant des données fournis par <a href="https://www.ecoledirecte.com" target="_blank">EcoleDirecte</a>, héritant ainsi de leurs mentions légales qui sont présentées ci-dessous :</h4>
         <li className="legal-content">Les données personnelles sécurisées figurant sur ce site Internet concernent des élèves et les familles, et sont fournies par le logiciel Charlemagne des établissements scolaires au sein desquels ceux-ci sont scolarisés.</li>
         <li className="legal-content">EcoleDirecte ne collecte aucune donnée personnelle directement sur le site Internet, ni cookie, à l’exception des email et téléphone mobile, utilisés EXCLUSIVEMENT pour la récupération des identifiants.</li>
         <li className="legal-content">Ces établissements scolaires se sont engagés à apporter tous leurs soins dans la qualité des informations diffusées. Il s’agit toutefois d’indications qui, en aucun cas, ne pourraient faire foi en lieu et place des documents usuels (bulletins de notes, relevés de notes, relevés d’absences et de sanctions).</li>
@@ -36,7 +38,7 @@ export default function Policy({ onCloseNavigateURL }) {
         <li className="legal-content">Le responsable du traitement de ces données est l’établissement scolaire qui a procédé à leur saisie. Le responsable de leur sécurisation sur notre plateforme EcoleDirecte en qualité de sous-traitant est : Société Aplim ZA Les Côtes 73190 Saint Jeoire Prieuré, rgpd@aplim.fr.</li>
         <li className="legal-content">En application des articles 15, 16, 17 et 18 du Règlement du Parlement européen et du Conseil du 27 avril 2016, et de la Loi n° 78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés, vous disposez d'un droit d'accès, de rectification, d'effacement des données collectées, et d'un droit de limitation du traitement des données personnelles que vous pouvez exercer auprès du responsable du traitement (à savoir l’établissement scolaire responsable du traitement) qui nous répercutera votre demande à fins d’exécution sur le site EcoleDirecte.</li>
         <li className="legal-content">En toute hypothèse, il vous est possible d'introduire une réclamation auprès de la Commission Nationale de l'Informatique et des Libertés si vous vous estimez lésé par le traitement de vos données personnelles par l’établissement scolaire.</li>
-        <li className="legal-content">L'activation de l'amélioration de l'accessibilité des personnes déficientes visuelles utilise la police de caractères <a className="external-link" href="https://luciole-vision.com/">Luciole</a>. Cette police de caractères est distribuée gratuitement sous Licence publique <a className="external-link" href="https://creativecommons.org/licenses/by/4.0/legalcode.fr">Creative Commons Attribution 4.0 International</a> : Luciole © Laurent Bourcellier & Jonathan Perez</li>
+        <li className="legal-content">L'activation de l'amélioration de l'accessibilité des personnes déficientes visuelles utilise la police de caractères <a className="external-link" href="https://luciole-vision.com/" target="_blank">Luciole</a>. Cette police de caractères est distribuée gratuitement sous Licence publique <a className="external-link" href="https://creativecommons.org/licenses/by/4.0/legalcode.fr" target="_blank">Creative Commons Attribution 4.0 International</a> : Luciole © Laurent Bourcellier & Jonathan Perez</li>
         <hr />
         <h3 className="part-header" >Conditions d'utilisations</h3>
         <h4 className="sub-header">1. Général</h4>
@@ -52,17 +54,18 @@ export default function Policy({ onCloseNavigateURL }) {
         <h4 className="sub-header">6. Liens, sites et services tiers</h4>
         <li className="legal-content">Le service peut contenir des liens vers des sites Web tiers, des services, ou d'autres événements ou activités qui ne sont ni détenus ni contrôlés par Ecole Directe Plus. Nous n'approuvons ni n'assumons aucune responsabilité pour ces sites, informations, matériaux, ou services tiers. Si vous accédez à un site Web, un service ou un contenu tiers d'Ecole Directe Plus, vous comprenez que ces conditions générales et notre politique de confidentialité ne s'appliquent pas à votre utilisation de ces sites. Vous reconnaissez et acceptez expressément qu'Ecole Directe Plus ne sera pas responsable, directement ou indirectement, de tout dommage ou perte résultant de votre utilisation de tout site Web, service ou contenu tiers.</li>
         <h4 className="sub-header">7. Résiliation</h4>
-        <li className="legal-content">Ecole Directe Plus peut résilier votre accès et votre utilisation du service immédiatement à tout moment, pour quelque raison que ce soit, et à ce moment-là, vous n'aurez plus le droit d'utiliser le service.</li>
+        <li className="legal-content">Ecole Directe Plus peut résilier votre accès et votre utilisation du service à tout moment, pour quelque raison que ce soit, et à ce moment-là, vous n'aurez plus le droit d'utiliser le service. Attention, même si vous n'avez plus accès à ED+, cela ne signifie pas que votre accès à EcoleDirecte ait été résilié, les deux sites étant des entitées indépendantes.</li>
         <hr />
         <h3 className="part-header">Crédits</h3>
         <li className="legal-content">Ecole Directe Plus est l'initiative du groupuscule Magic-Fish :<br /><br />
             Développeurs principaux :<br />
             - {leadDeveloppers[firstLeadDeveloppersDisplayedIdx]}<br />
-            - {leadDeveloppers[leadDeveloppers.length-firstLeadDeveloppersDisplayedIdx-1]}<br /><br />
+            - {leadDeveloppers[leadDeveloppers.length - firstLeadDeveloppersDisplayedIdx - 1]}<br /><br />
 
             Autres contributeurs :<br />
             - Akash Banchhor<br />
-            - Oeildelynx<br /><br />
+            - Oeildelynx<br />
+            - xav35000<br /><br />
 
             APIs et services tiers :<br />
             - EcoleDirecte<br />
@@ -102,8 +105,8 @@ export default function Policy({ onCloseNavigateURL }) {
             The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
             The Software is provided “as is”, without warranty of any kind, express or implied, including but not limited to the warranties of merchantability, fitness for a particular purpose and noninfringement. In no event shall the authors or copyright holders X be liable for any claim, damages or other liability, whether in an action of contract, tort or otherwise, arising from, out of or in connection with the software or the use or other dealings in the Software.</li>
-        <li className="legal-content" style={{listStyle: "none", marginBlock: "20px 2px"}}>Dernière révision le 4 févirer 2024</li>
-        <li className="legal-content" style={{listStyle: "none", marginBlock: "2px 20px"}}>Nous contacter : <a href="mailto:contact@ecole-directe.plus" style={{ color: "rgb(var(--text-color-alt))"}} >contact@ecole-directe.plus</a></li>
+        <li className="legal-content" style={{ listStyle: "none", marginBlock: "20px 2px" }}>Dernière révision le 26 mars 2024</li>
+        <li className="legal-content" style={{ listStyle: "none", marginBlock: "2px 20px" }}>Nous contacter : <a href="mailto:contact@ecole-directe.plus" style={{ color: "rgb(var(--text-color-alt))" }} >contact@ecole-directe.plus</a></li>
         <li id="copyleft-container">Copyleft <Copyleft id="copyleft" alt="Copyleft" width="13" height="13" /> {new Date().getFullYear()} Ecole Directe Plus</li>
     </ul>
 
