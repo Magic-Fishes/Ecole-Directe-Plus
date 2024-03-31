47cb88ff20f43a688a7464f7437d3a30e794bc63
Saumon Brulé <96207807+saumon-brule@users.noreply.github.com>
2024-03-31 23:23:02 +0200

    feat: adding timer for notification and setting the edp-unblock notification to infinite + dcoumentation of addNotification function

--- src/components/generic/PopUps/Notification.jsx
+++ src/components/generic/PopUps/Notification.jsx
@@ -42,20 +42,30 @@ export default function DOMNotification({ children }) {
         }, delay)
     }
 
-    function addNotification(newNotificationContent, customClass = "") {
+    function addNotification(newNotificationContent, {customClass = "", timer = 3000}) {
+        /** This function will summon a notification
+         * @param newNotificationContent content (in jsx) of the notification
+         * @param customClass class you can add to the notification pop-up to change the default style
+         * @param timer time in ms before the notification start closing OR infinite to make the notification stay forever 
+         * WARNING : the notification context being outside of the router, <Link/> component and hooks like useNavigate from react-router-dom will not work. (It would for useNavigate if the function called is declared in the router)
+         */
         const newNotification = {
-            key: currentNotificationId.current,
-            content: newNotificationContent,
-            isClosing: 0,
-            creationTime: new Date().getTime(),
+            key: currentNotificationId.current, // key for react list handling (should be different for each notification but sometime isn't for strange reasons)
+            content: newNotificationContent, // content of the notification
+            isClosing: 0, // 0 if the notification isn't closing, 1 is if it is a classic closing and 2 if it is a fat-closing (when the user manually clic on the arrow)
+            creationTime: new Date().getTime(), // timer of creation of the notification, used to delete it at the right time
             customClass,
-            hoverred: false,
+            timer,
+            hoverred: false, // boolean indicating if the notification is hover or not
         }
+
         currentNotificationId.current++;
         setNotificationList(() => [newNotification].concat(...notificationList))
-        setTimeout(() => {
-            closeNotification(newNotification)
-        }, 3000);
+        if (timer !== "infinite") {
+            setTimeout(() => {
+                closeNotification(newNotification)
+            }, timer);
+        }
     }
 
     let notificationToRemove = []
@@ -71,7 +81,7 @@ export default function DOMNotification({ children }) {
 
     function handleMouseLeave(notification) {
         notification.hovered = false;
-        if (notification.creationTime < new Date().getTime() - 3500) {
+        if (notification.timer !== "infinite" || notification.creationTime < new Date().getTime() - (notification.timer + 500)) {
             closeNotification(notification, false, 1000)
             notification.creationTime = new Date().getTime() - 1500
         }
@@ -85,7 +95,7 @@ export default function DOMNotification({ children }) {
         <notificationContext.Provider value={addNotification} >
             <div id="notifications-container">
                 {notificationList.map((el, i) => {
-                    if (el.creationTime > new Date().getTime() - 3500 || el.hovered) {
+                    if (el.timer === "infinite" || el.creationTime > new Date().getTime() - (el.timer + 500) || el.hovered) {
                         return (
                             <div className={`pop-up-notification ${el.isClosing === 1 ? " closing" : (el.isClosing === 2 ? " fast-closing" : "")} ${el.customClass}`} key={el.key} onMouseEnter={() => handleMouseEnter(el)} onMouseLeave={() => handleMouseLeave(el)} >
                                 <DropDownArrow className="notification-close-arrow" onClick={() => { closeNotification(el, true) }} />
