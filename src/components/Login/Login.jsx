57035119b8b78e249c3f92acc653bfa79fba2753
Truite Séchée <truite.berthail@gmail.com>
2024-04-01 00:04:11 +0200

    (again) hide EDP-Unblock ad banner

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -11,35 +11,21 @@ import GithubLink from "../generic/buttons/GithubLink";
 import EDPLogo from "../graphics/EDPLogo";
 import EDPLogoFullWidth from "../graphics/EDPLogoFullWidth";
 
-if (sessionStorage.getItem('april') === "true"){
-    import("./april.css").then((something) => {
-        console.log("April fools styles loaded");
-    })
-}
-
 import "./Login.css";
 import ExtensionIcon from "../graphics/ExtensionIcon";
 
 export default function Login({ keepLoggedIn, setKeepLoggedIn, A2FInfo, setRequireA2F, bufferUserIds, fetchLogin, logout, loginFromOldAuthInfo, currentEDPVersion }) {
     const location = useLocation();
 
-    if (localStorage.userSettings) {
-        if (((JSON.parse(localStorage.userSettings)[0].displayTheme) !== "dark") && (localStorage.getItem('april') === "true")) {
-            document.body.style.backgroundColor = "white";
-        } else {
-            document.body.style.backgroundColor = "rgb(var(--background-color-0))" ;
-        }
-    }
-    
-     // JSX
+    // JSX
     return (
         <div id="login">
             <EDPLogoFullWidth className="login-logo" id="outside-container" alt="Logo Ecole Directe Plus" />
-            <Link to="/edp-unblock" className="temp-disclaimer" id="edp-unblock-ad">
+            {/* <Link to="/edp-unblock" className="temp-disclaimer" id="edp-unblock-ad">
                 <ExtensionIcon className="extension-icon" />
                 <span>Installez l'extension EDP Unblock pour accéder en continu à Ecole Directe Plus ! <span className="edpu-explanation">Ecole Directe Plus a besoin de son extension pour accéder au contenu fourni par l’API d’EcoleDirecte.</span></span>
                 <Link to="/edp-unblock#about">En savoir plus</Link>
-            </Link>
+            </Link> */}
             <span className="login-social">
                 <DiscordLink />
                 <GithubLink />
@@ -50,9 +36,9 @@ export default function Login({ keepLoggedIn, setKeepLoggedIn, A2FInfo, setRequi
                 <h1>Connexion</h1>
                 <LoginForm keepLoggedIn={keepLoggedIn} setKeepLoggedIn={setKeepLoggedIn} A2FInfo={A2FInfo} setRequireA2F={setRequireA2F} bufferUserIds={bufferUserIds} fetchLogin={fetchLogin} logout={logout} loginFromOldAuthInfo={loginFromOldAuthInfo} />
             </div>
-            {/* <p className="policy">
+            <p className="policy">
                 En vous connectant, vous confirmez avoir lu et accepté notre <Link to="#policy" replace={true} className="policy-link" id="legal-notice">Politique de confidentialité et Conditions d'utilisation</Link>.
-            </p> */}
+            </p>
             {location.hash === "#policy" && <Policy onCloseNavigateURL={""} />}
             <EDPVersion currentEDPVersion={currentEDPVersion} />
             <Outlet />
