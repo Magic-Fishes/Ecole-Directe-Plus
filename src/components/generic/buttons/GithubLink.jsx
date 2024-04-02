c9d74ff21f78672f9f6168053941073283a720a3
Truite Séchée <truite.berthail@gmail.com>
2024-04-02 23:57:33 +0200

    refactor(EdpUnblock): better exception management, improving transparency and clarity of EDP-Unblock related information, fix(GithubLink): fix Login link

--- src/components/generic/buttons/GithubLink.jsx
+++ src/components/generic/buttons/GithubLink.jsx
@@ -1,10 +1,10 @@
 import GithubLogo from "../../graphics/GithubLogo";
 import "./SocialButtonLink.css";
 
-export default function GithubLink({ ...props }) {
+export default function GithubLink({ githubRepoHref="https://github.com/Magic-Fishes/Ecole-Directe-Plus", ...props }) {
     return (
         <>
-            <a className="social-button-link" href="https://github.com/Magic-Fishes/Ecole-Directe-Plus-Unblock" target="_blank" {...props} ><GithubLogo /></a>
+            <a className="social-button-link" href={githubRepoHref} target="_blank" {...props} ><GithubLogo /></a>
         </>
     )
 }
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -115,3 +115,26 @@ export function getBrowser() { // I didn't check all browsers, see : https://dev
         "Safari" // not verified
     )
 }
+
+export function getOS() {
+    const userAgent = window.navigator.userAgent,
+        platform = window.navigator?.userAgentData?.platform || window.navigator.platform,
+        macosPlatforms = ['macOS', 'Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+        windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+        iosPlatforms = ['iPhone', 'iPad', 'iPod'];
+    let os = null;
+
+    if (macosPlatforms.indexOf(platform) !== -1) {
+        os = 'MacOS';
+    } else if (iosPlatforms.indexOf(platform) !== -1) {
+        os = 'iOS';
+    } else if (windowsPlatforms.indexOf(platform) !== -1) {
+        os = 'Windows';
+    } else if (/Android/.test(userAgent)) {
+        os = 'Android';
+    } else if (/Linux/.test(platform)) {
+        os = 'Linux';
+    }
+
+    return os;
+}
