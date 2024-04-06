35b619950f2baaf113b4d10c6f6fa2652617aa5a
name <leopaulchutell@gmail.com>
2024-04-06 09:21:24 +0200

    Create mainpage discord/github Logo imported change dark shadow

--- src/components/Mainpage/MainPage.jsx
+++ src/components/Mainpage/MainPage.jsx
@@ -4,7 +4,11 @@ import Login from "../../components/Login/Login";
 import "./MainPage.css";
 import "../../../src/App.css"
 import EDPLogo from "../graphics/EDPLogo";
-import EDPLogoFullWidth from "../graphics/EDPLogoFullWidth.jsx"
+import DiscordFullLogoSmall from "../graphics/DiscordFullLogoSmall.jsx";
+import GitHubFullLogo from "../graphics/GitHubFullLogo.jsx";
+
+
+
 
 
 export default function MainPage() {
@@ -62,7 +66,7 @@ export default function MainPage() {
         <div className="main-page">
         <section id="nav-bar">
             <header className="top-section">
-                <nav className={`nav-bar-content ${isMenuOpen ? 'mobile-menu' : ''}`}>
+                <nav className="nav-bar-content">
                 <div className="nav-logo">
                     <a href=""><EDPLogo height="15" className="EDPLogo"/>Ecole Directe Plus</a>
                 </div>
@@ -74,13 +78,14 @@ export default function MainPage() {
                         <span class="spinner diagonal part-2"></span>
                     </label>
                 </div>
+                
                     <div className="nav-links-container">
                         <div className="inline">
-                            <div className="nav-links">
+                            <div className={`nav-links ${isMenuOpen ? 'mobile-menu' : ''}`}>
                                 
-                                    <a href="#" className={`link ${activeLink === 'Home' ? 'nav-active' : ''}`} onClick={() => handleSetActiveLink('Home')}>Home</a>
-                                    <a href="#" className={`link ${activeLink === 'Community' ? 'nav-active' : ''}`} onClick={() => handleSetActiveLink('Community')}>Community</a>
-                                    <a href="#" className={`link ${activeLink === 'Open-Source' ? 'nav-active' : ''}`} onClick={() => handleSetActiveLink('Open-Source')}>Open-Source</a>
+                                    <a href="#banner" className={`link ${activeLink === 'Home' ? 'nav-active' : ''}`} onClick={() => handleSetActiveLink('Home')}>Home</a>
+                                    <a href="#community" className={`link ${activeLink === 'Community' ? 'nav-active' : ''}`} onClick={() => handleSetActiveLink('Community')}>Community</a>
+                                    <a href="#open-source" className={`link ${activeLink === 'Open-Source' ? 'nav-active' : ''}`} onClick={() => handleSetActiveLink('Open-Source')}>Open-Source</a>
                                     <a href="#" className={`link ${activeLink === 'The Extension' ? 'nav-active' : ''}`} onClick={() => handleSetActiveLink('The Extension')}>The Extension</a>
                     
                             </div>
@@ -88,7 +93,7 @@ export default function MainPage() {
                     </div>
                     <div className="login-theme">
                         <div className="nav-login">
-                            <a href="/login">Login</a>
+                            <a href="/login">Log-In</a>
                         </div>
                         <div className="change-theme">
                             <button id="toggle-button" onClick={changeTheme}>
@@ -121,7 +126,7 @@ export default function MainPage() {
                             Respect de la Vie Privée
                         </h3>
                         <h6>
-                            Ecole Directe Plus s'engage à respecter votre vie privée, aucune donnée n'est sauvegardé sur les serveur d'Ecole Directe Plus. Tout transite sur les serveurs d'Ecole Directe classique.
+                            Ecole Directe Plus s'engage à respecter votre vie privée, aucune donnée n'est sauvegardé sur les serveur d'Ecole Directe Plus. Toutes les données transitent sur les serveurs d'Ecole Directe classique.
                         </h6>
                     </div>
                     <div className="ban-box-desing ban-intro">
@@ -137,19 +142,61 @@ export default function MainPage() {
                             Plateforme Éducative Open-Source.
                         </h3>
                         <h6>
-                            Ecole Directe Plus est un projet open-source, développé par une communauté de passionnés. Rejoignez-nous pour contribuer à l'amélioration continue de notre plateforme éducative. <a href="#" className="more-info">En savoir plus sur l'Open-Source</a>
+                            Ecole Directe Plus est un projet open-source, développé par une communauté de passionnés. Rejoignez-nous pour contribuer à l'amélioration continue de notre plateforme éducative. <a href="#open-source" className="more-info">En savoir plus sur l'Open-Source</a>
                         </h6>
                     </div>
                 </div>
             </div>
         </section>
-
-
-            
-            <div className="info-box">
-                {/* Contenu de la page */}
+        <section id="community">
+            <div className="com-box-container">
+                <div className="com-title box-desing-title">
+                    <h4>
+                        Community
+                    </h4>
+                </div>
+                <div className="presentation-box">
+                    <div className="box-desing">
+                        <h6>
+                            Ecole Directe Plus est le résultat d'une collaboration étroite et dynamique entre des développeurs passionnés et une communauté d'utilisateurs actifs. Notre objectif commun est de vous offrir constamment de nouvelles fonctionnalités pour améliorer votre expérience.
+                        </h6>
+                    </div>
+                    <div className="box-desing">
+                        <h6>
+                            Si vous souhaitez rejoindre notre communauté et contribuer à ce projet, nous avons un serveur Discord dédié. Les développeurs et les utilisateurs s'y retrouvent pour discuter des ajouts et des modifications à apporter. Nous serions ravis de vous accueillir et de travailler ensemble pour améliorer Ecole Directe Plus.
+                        </h6>
+                    </div>
+                    <div className="box-desing linked-button">
+                        <a target="_blank" href="https://discord.gg/AKAqXfTgvE"><DiscordFullLogoSmall/></a>
+                    </div>
+                </div>
             </div>
-            
+        </section>
+        <section id="open-source">
+            <div className="op-box-container">
+                <div className="box-desing-title">
+                    <h4>
+                        Open-Source
+                    </h4>
+                </div>
+                <div className="presentation-box">
+                    <div className="box-desing">
+                        <h6>
+                            Le projet Ecole Directe Plus est un logiciel Open Source, accessible à tous et offrant à chacun la possibilité de contribuer à son développement. Rejoignez notre communauté et aidez-nous à améliorer l'éducation pour tous !
+                        </h6>
+                    </div>
+                    <div className="box-desing">
+                        <h6>
+                            Rejoignez le GitHub officiel du projet Ecole Directe Plus pour contribuer à son développement. Proposez des fonctionnalités, signalez des bugs et collaborez avec d'autres développeurs sur la plateforme GitHub. Faites partie de la communauté et aidez à améliorer l'éducation pour tous !
+                        </h6>
+                    </div>
+                    <div className="box-desing linked-button">
+                        <a target="_blank" href="https://github.com/Magic-Fishes/Ecole-Directe-Plus"><GitHubFullLogo/></a>
+                    </div>
+
+                </div>
+            </div>
+        </section>
             <p className="policy">
                 En vous connectant, vous confirmez avoir lu et accepté notre <Link to="#policy" replace={true} className="policy-link" id="legal-notice">Politique de confidentialité et Conditions d'utilisation</Link>.
             </p>
