dc37fd8f77fa39523a4801860e9dfe90fd4ad672
saumon-brule <96207807+saumon-brule@users.noreply.github.com>
2024-05-17 23:35:20 +0200

    feat(upcoming assignments) : adding relative dates

--- src/components/app/Homeworks/Interrogation.jsx
+++ src/components/app/Homeworks/Interrogation.jsx
@@ -1,5 +1,6 @@
 import { useContext, useRef } from "react";
 import { textToHSL } from "../../../utils/utils"
+import { formatDateRelative } from "../../../utils/date"
 import CheckBox from "../../generic/UserInputs/CheckBox"
 import { AppContext } from "../../../App";
 import { useNavigate } from "react-router-dom";
@@ -64,7 +65,7 @@ export default function Interrogation({ task }) { // This component only exists 
         ? <div tabIndex="0" role="a" onKeyDown={(e) => handleKeyDown(e, task.date, task.id)} onClick={() => handleClick(task.date, task.id)} className="upcoming-assignments" style={{ backgroundColor: taskColor }}>
             <CheckBox onChange={() => { checkTask(task.date, task.index) }} ref={taskCheckboxRef} checked={currentSortedHomeworks[task.date][task.index].isDone} id={`${task.id}-upcoming-assignments"`} onMouseEnter={() => isMouseInCheckBoxRef.current = true} onMouseLeave={() => isMouseInCheckBoxRef.current = false} style={{"backgroundImage": currentSortedHomeworks[task.date][task.index].isDone ? `url("data:image/svg+xml,%3Csvg width='126' height='90' viewBox='0 0 126 90' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M3.00999 29.4982L4.7539 27.7442C8.77118 23.7036 15.3475 23.8312 19.205 28.0246L47.306 58.5723C48.849 60.2496 51.4795 60.3007 53.0864 58.6844L108.318 3.13256C112.228 -0.799963 118.591 -0.799963 122.501 3.13256L122.99 3.62419C126.868 7.5247 126.868 13.8249 122.99 17.7254L52.9741 88.147C51.4102 89.72 48.8649 89.72 47.301 88.147L3.00999 43.5994C-0.868052 39.6989 -0.868055 33.3987 3.00999 29.4982Z' fill='rgb(0, 0, 0, .2)'/%3E%3C/svg%3E")` : ""}} />
             <span>{task.subject}</span>
-            <span>{task.date}</span>
+            <span>{formatDateRelative(new Date(task.date))}</span>
         </div>
         : <div className="dummy-interrogation" />
 }
