2ef63a3d723d6ae154eb346a68bfcb85b06c6e92
M6a5x98 <m6a5x98@outlook.com>
2024-10-17 22:27:53 +0200

    Update FileComponent.jsx
    Simplified return by changing the download icon and not the whole file.
    Used the getOS function from src/utils/utils.js to check if on iOS

--- src/components/generic/FileComponent.jsx
+++ src/components/generic/FileComponent.jsx
@@ -1,5 +1,6 @@
 import { useRef } from "react"
 
+import { getOs } from "../../utils/utils"
 import "./FileComponent.css"
 import DefaultFileIcon from "../graphics/file/DefaultFileIcon";
 import DownloadIcon from "../graphics/DownloadIcon"
@@ -31,16 +32,7 @@ export default function FileComponent({ file, ...props }) {
     }
 
     function iOS() {
-        return [
-          'iPad Simulator',
-          'iPhone Simulator',
-          'iPod Simulator',
-          'iPad',
-          'iPhone',
-          'iPod'
-        ].includes(navigator.platform)
-        // iPad on iOS 13 detection
-        || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+        return getOs() === "iOS"
       }
 
     const quitActive = (e) => {
@@ -51,12 +43,9 @@ export default function FileComponent({ file, ...props }) {
             timeOutRef.current = null
         }
     }
-return !iOS() ? <div className="file-component" onMouseDown={handleMouseDown} onMouseUp={quitActive} onMouseLeave={quitActive} {...props}>
-<DefaultFileIcon extension={file.extension} className="file-icon"/>
-<span className="file-name" >{file.name}.{file.extension}</span>
-</div> : <div className="file-component" onMouseDown={handleMouseDown} onMouseUp={quitActive} onMouseLeave={quitActive} {...props}>
+    return <div className="file-component" onMouseDown={handleMouseDown} onMouseUp={quitActive} onMouseLeave={quitActive} {...props}>
         <DefaultFileIcon extension={file.extension} className="file-icon"/>
         <span className="file-name" >{file.name}.{file.extension}</span>
-        <DownloadIcon onMouseDown={handleMouseDown} onMouseUp={quitActive} onMouseLeave={quitActive} width="20" height="20"/>
+        {iOS() ? <DownloadIcon onMouseDown={handleMouseDown} onMouseUp={quitActive} onMouseLeave={quitActive} width="20" height="20"/> : <div></div>}
     </div>
 }
