c2d2744b44baa7654c9fd0e664cb549c186babeb
Super-Code774 <kimonos.potiers-0f@icloud.com>
2025-01-20 17:15:44 +0100

    Update proxy_server.js

--- server/proxy_server.js
+++ server/proxy_server.js
@@ -1,58 +1,62 @@
-const express = require('express');
-const morgan = require("morgan");
-const cors = require("cors");
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const fs = require("fs");
-const https = require('https');
+     const express = require('express');
+     const morgan = require("morgan");
+     const cors = require("cors");
+     const { createProxyMiddleware } = require('http-proxy-middleware');
+     const fs = require("fs");
+     const https = require('https');
 
-// Creating express server
-const app = express();
+     // Creating express server
+     const app = express();
 
-const PORT = 3000;
-const HOST = "localhost";
+     const PORT = 3000;
+     const HOST = "localhost";
 
-app.use(morgan('dev'));
-app.use(cors());
+     app.use(morgan('dev'));
 
-// SSL certificate and private key
-const SSL_CERTIFICATE_PATH = "[FULLKEY_PEM]";
-const SSL_PRIVATE_KEY_PATH = "[PRIVKEY_PEM]";
+     const corsOptions = {
+       origin: 'https://www.ecoledirecte.com', // Allow specific origin
+       methods: ['GET', 'POST', 'PUT', 'DELETE'],
+       credentials: true
+     };
 
-const sslOptions = {
-	cert: fs.readFileSync(SSL_CERTIFICATE_PATH),
-	key: fs.readFileSync(SSL_PRIVATE_KEY_PATH)
-};
+     app.use(cors(corsOptions));
 
+     // SSL certificate and private key
+     const SSL_CERTIFICATE_PATH = "[FULLKEY_PEM]";
+     const SSL_PRIVATE_KEY_PATH = "[PRIVKEY_PEM]";
 
-// Proxy endpoints
-app.all('/proxy', (req, res, next) => {
-	createProxyMiddleware(
-		{
-			target: req.query.url,
-			onProxyReq: (proxyReq, req, res) => {
-				console.log("redirected to", req.query.url)
-				console.log(req.method);
-				proxyReq.method = req.method;
-				proxyReq.path = req.query.url;
+     const sslOptions = {
+       cert: fs.readFileSync(SSL_CERTIFICATE_PATH),
+       key: fs.readFileSync(SSL_PRIVATE_KEY_PATH)
+     };
 
-				proxyReq.setHeader("origin", "https://www.ecoledirecte.com");
-				proxyReq.setHeader("referer", "https://www.ecoledirecte.com/");
-				proxyReq.setHeader("x-forwarded-server", "api.ecoledirecte.com");
-				proxyReq.setHeader("x-forwarded-host", "api.ecoledirecte.com");
-				proxyReq.setHeader("x-forwarded-for", "56.15.23.89");
-				proxyReq.setHeader("host", new URL(req.query.url).host);
-			},
-			onError: (err, req, res) => {
-				console.error('Proxy error:', err);
-				res.status(500).send('Proxy error');
-			},
-			changeOrigin: true,
-		})(req, res, next);
-});
+     // Proxy endpoints
+     app.all('/proxy', (req, res, next) => {
+       createProxyMiddleware({
+         target: req.query.url,
+         onProxyReq: (proxyReq, req, res) => {
+           console.log("redirected to", req.query.url);
+           console.log(req.method);
+           proxyReq.method = req.method;
+           proxyReq.path = req.query.url;
 
+           proxyReq.setHeader("origin", "https://www.ecoledirecte.com");
+           proxyReq.setHeader("referer", "https://www.ecoledirecte.com/");
+           proxyReq.setHeader("x-forwarded-server", "api.ecoledirecte.com");
+           proxyReq.setHeader("x-forwarded-host", "api.ecoledirecte.com");
+           proxyReq.setHeader("x-forwarded-for", "56.15.23.89");
+           proxyReq.setHeader("host", new URL(req.query.url).host);
+         },
+         onError: (err, req, res) => {
+           console.error('Proxy error:', err);
+           res.status(500).send('Proxy error');
+         },
+         changeOrigin: true,
+       })(req, res, next);
+     });
 
-const httpsServer = https.createServer(sslOptions, app);
+     const httpsServer = https.createServer(sslOptions, app);
 
-httpsServer.listen(PORT, HOST, () => {
-	console.log(`Starting HTTPS Proxy at ${HOST}:${PORT}`);
-});
+     httpsServer.listen(PORT, HOST, () => {
+       console.log(`Starting HTTPS Proxy at ${HOST}:${PORT}`);
+     });
