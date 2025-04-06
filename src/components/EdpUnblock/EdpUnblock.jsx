95b8c6f8768abb48c5b99f1d71893263f0f0a193
saumon-brule <96207807+saumon-brule@users.noreply.github.com>
2025-04-07 01:59:11 +0200

    feat(/edp-unblock): add auto URL update for edpu on firefox

--- src/components/EdpUnblock/EdpUnblock.jsx
+++ src/components/EdpUnblock/EdpUnblock.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useLocation, Link } from "react-router-dom";
 import { getBrowser, getOS } from "../../utils/utils";
 import { getZoomedBoudingClientRect } from "../../utils/zoom";
@@ -17,7 +17,7 @@ import BadgeCheck from "../graphics/BadgeCheck";
 
 import "./EdpUnblock.css";
 
-const browserLogosInfos = {
+const browserInfos = {
     [Browsers.FIREFOX]: {
         logo: <FirefoxLogo />,
         available: true,
@@ -64,9 +64,22 @@ const compatibilityCondition = (
     || (userOS === OperatingSystems.MACOS && userBrowser === Browsers.SAFARI)
 );
 
+async function getFirefoxUrl() {
+    return fetch("https://unblock.ecole-directe.plus/update.json")
+        .then((response) => response.json())
+        .then((response) => {
+            const updates = response.addons["{edpu-firefox-self-host@ecole-directe.plus}"].updates;
+            const url = updates[updates.length - 1].update_link;
+            return url;
+        })
+}
+
+const userBrowserInfo = browserInfos?.[userBrowser];
+
 export default function EdpUnblock({ isEDPUnblockActuallyInstalled }) {
     const location = useLocation();
 
+    const [downloadUrl, setDownloadUrl] = useState(userBrowserInfo.url);
     const aboutRef = useRef(null);
     const aboutButtonRef = useRef(null);
     const heroBannerRef = useRef(null);
@@ -75,6 +88,16 @@ export default function EdpUnblock({ isEDPUnblockActuallyInstalled }) {
             aboutRef.current.scrollIntoView({ block: "start", inline: "nearest", behavior: "smooth" });
         }
     }
+
+    useEffect(() => {
+        if (userBrowser == Browsers.FIREFOX)
+        {
+
+            getFirefoxUrl().then((url) => {
+                setDownloadUrl(url);
+            });
+        }
+    }, []);
 
     useEffect(() => {
         function handleKeyDown(event) {
@@ -127,8 +150,12 @@ export default function EdpUnblock({ isEDPUnblockActuallyInstalled }) {
                     </div>
                     <p>Ecole Directe Plus a besoin de cette extension de navigateur pour <span style={{ fontWeight: "800" }}>fonctionner correctement</span> et accéder à l’API d’EcoleDirecte.</p>
                     {compatibilityCondition && (userOS !== OperatingSystems.IOS ? <><p>Malheureusement, l'extension Ecole Directe Plus Unblock n'est pas disponible sur votre navigateur. 😥</p><p>S'il vous plaît considérez l'usage d'un navigateur compatible comme le <a href={userOS === OperatingSystems.IOS ? "https://apps.apple.com/app/id1484498200" : "https://play.google.com/store/apps/details?id=org.mozilla.firefox"} className="suggested-browser" target="_blank">{userOS === OperatingSystems.IOS ? "navigateur Orion" : "navigateur Firefox"}</a>.</p></> : <p>Malheureusement, l'extension Ecole Directe Plus Unblock n'est pas compatible avec les navigateurs sur iOS et iPadOS. S'il vous plaît, considérez l'usage d'un autre appareil avec un système d'exploitation compatible comme un ordinateur sous Windows ou Linux, ou un appareil mobile sous Android.</p>)}
-                    <a href={browserLogosInfos[userBrowser] && browserLogosInfos[userBrowser].url} target={userBrowser === Browsers.FIREFOX ? "_self" : "_blank"} className={`edpu-download-link ${compatibilityCondition ? "disabled" : ""} ${browserLogosInfos[userBrowser] && browserLogosInfos[userBrowser].available ? "available" : "unavailable"}`}>
-                        {browserLogosInfos[userBrowser] && browserLogosInfos[userBrowser].logo}
+                    <a
+                        href={downloadUrl}
+                        target={userBrowser === Browsers.FIREFOX ? "_self" : "_blank"}
+                        className={`edpu-download-link ${compatibilityCondition && downloadUrl === undefined ? "disabled" : ""} ${userBrowserInfo.available && downloadUrl !== undefined ? "available" : "unavailable"}`}
+                    >
+                        {userBrowserInfo.logo}
                         {isEDPUnblockActuallyInstalled
                             ? <span>Extension installée</span>
                             : (compatibilityCondition ? <span>Navigateur incompatible</span> : <span>Ajouter l’extension</span>)
